27eda07cd281d248eb708d8169ee4364e82ae91a
片山翼 <denon38msn@gmail.com>
2020-12-12 19:27:05 +0900

    worning_delete

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,6 +1,6 @@
-import firebase from "firebase";
-import "firebase/auth";
-import 'firebase/firestore';
+import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/firestore"
 
 if(!firebase.apps.length){
   firebase.initializeApp({
